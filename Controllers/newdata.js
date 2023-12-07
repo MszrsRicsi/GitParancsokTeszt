73e20355c9cbc8745616542b4a18923e50f18744
MszrsRicsi <meszarosrichard@turr.hu>
2023-12-07 09:17:16 +0100

    NewData oldal és kontrollere

---
+++ Controllers/newdata.js
@@ -0,0 +1,37 @@
+function setMaxDate(){
+    let date = document.querySelector('#date');
+    date.max = new Date().toISOString().split("T")[0];
+}
+
+function getToday(){
+    setTimeout(()=>{setMaxDate()}, 500);
+}
+
+function addData(){
+
+    let date = document.querySelector('#date');
+    let amount = document.querySelector('#amount');
+    let income = document.querySelector("#type");
+    let title = document.querySelector("#title");
+
+    if (date.value == "" || amount.value == 0 || income.selectedIndex == 0 || title.value == ""){
+        showMessage("Nem adtál meg minden adatot!");
+    }
+    else
+    {
+        let data = {
+            userID: loggedUser.ID,
+            date: date.value,
+            amount: amount.value,
+            type: income.value,
+            tag: title.value
+        };
+        axios.post(`${serverURL}/items`, data).then((res)=>{
+            alert('Adatok rögzítve!');
+            date.value = null;
+            amount.value = 0;
+            income.selectedIndex = 0;
+            title.value = "";
+        });
+    }
+}
